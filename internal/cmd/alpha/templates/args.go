7f9bea8b908b35ec32cc87698ccb978b22eff12b
pPrecel <filip.strozik@outlook.com>
2025-02-13 09:38:35 +0100

    improve optional resource name arg

--- internal/cmd/alpha/templates/args.go
+++ internal/cmd/alpha/templates/args.go
@@ -10,10 +10,10 @@ import (
 func AssignOptionalNameArg(name *string) func(*cobra.Command, []string) error {
 	return func(_ *cobra.Command, args []string) error {
 		if len(args) > 1 {
-			return fmt.Errorf("accepts at most one argument, received %d", len(args))
+			return fmt.Errorf("accepts at most one resource name as argument, received %d", len(args))
 		}
 		if len(args) == 1 {
-			*name = args[1]
+			*name = args[0]
 		}
 
 		return nil

dcb7801637d058abc5e16948522af3cd627d0394
pPrecel <filip.strozik@outlook.com>
2025-11-20 13:56:37 +0100

    do

--- internal/clierror/check.go
+++ internal/clierror/check.go
@@ -9,7 +9,17 @@ import (
 // Check prints error and executes os.Exit(1) if the error is not nil
 func Check(err Error) {
 	if err != nil {
-		out.Errln(err.String())
+		out.Errln(withHelpHint(err).String())
 		os.Exit(1)
 	}
+}
+
+func withHelpHint(err Error) Error {
+	clierr := err.(*clierror)
+	clierr.hints = append(
+		clierr.hints,
+		"use --help to see available commands and examples",
+	)
+
+	return clierr
 }
